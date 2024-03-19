657854b0d0fc60c0d41805af754a62188ddfb2a4
Timothy Arnold <timaeos@proton.me>
2024-03-19 16:28:45 -0500

    [bugfix] Makes `albumId` get set to `item.Id` if `item.AlbumId` doesn't exist.
    This is because `albumId` is often expected to be there in home-route views but `mostPlayed` view defaults to Songs on jellyfin which don't necessarily have an `albumId`

--- src/renderer/api/jellyfin/jellyfin-normalize.ts
+++ src/renderer/api/jellyfin/jellyfin-normalize.ts
@@ -134,7 +134,7 @@ const normalizeSong = (
             imageUrl: null,
             name: entry.Name,
         })),
-        albumId: item.AlbumId,
+        albumId: item.AlbumId ? item.AlbumId : item.Id,
         artistName: item?.ArtistItems?.[0]?.Name,
         artists: item?.ArtistItems?.map((entry) => ({
             id: entry.Id,
